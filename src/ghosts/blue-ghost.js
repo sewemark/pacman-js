f0f05b2af340de64e145edf0074707a451309336
sewemark <sewerynjakub.markowicz@gmail.com>
2018-04-09 20:56:10 +0200

    Game class refactored

---
+++ src/ghosts/blue-ghost.js
@@ -0,0 +1,41 @@
+import ActorDefinitions from '../map-definitions/map-config';
+import Ghost from './ghost';
+
+var RedGhost = (function() {
+  "use strict";
+
+  var priv = new WeakMap();
+
+  var  _ = function (inst) {
+    return priv.get(inst);
+  }
+
+  function RedGhostConstructor(ghostCollisionStrategy, mapManager) {
+
+    var data = mapManager.getNextTripForGhost(ActorDefinitions.REDGHOST);
+
+    var privMembers = {
+      mapManager: mapManager,
+      initData: data,
+      position: data.initaliGhostPosition,
+      path: data.path,
+      ghostCollisionStrategy: ghostCollisionStrategy,
+      newPositions: [],
+      GHOST: ActorDefinitions.REDGHOST,
+    };
+
+    Ghost.call(this, ghostCollisionStrategy, mapManager, privMembers);
+    priv.set(this, privMembers);
+
+  }
+
+  RedGhostConstructor.prototype = Object.create(Ghost.prototype);
+  RedGhostConstructor.prototype.constructor = RedGhostConstructor;
+
+  return RedGhostConstructor;
+
+})();
+
+
+export default RedGhost;
+
